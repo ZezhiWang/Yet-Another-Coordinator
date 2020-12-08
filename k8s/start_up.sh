a7d2f6f7626b500289bd100253372ab50cad5d3f
ZezhiWang <zezhi_wang@brown.edu>
2020-12-08 04:22:24 -0500

    add apply permission to start up script

--- k8s/start_up.sh
+++ k8s/start_up.sh
@@ -6,6 +6,9 @@ kubectl apply -f yac_namespace.json
 # establish namespace context
 kubectl config set-context --current --namespace=yac
 
+# k8s client permission
+kubectl apply -f client_perm.yaml
+
 # create deployment
 kubectl apply -f coordinator_deployment.yaml
 
